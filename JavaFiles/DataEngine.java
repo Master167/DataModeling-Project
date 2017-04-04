b8871d594f0d44b2ee15cb2c9e95d28abee5eb16
Michael Frederick <master16789@gmail.com>
2017-04-03 20:53:00 -0400

    Check for directories

--- JavaFiles/DataEngine.java
+++ JavaFiles/DataEngine.java
@@ -1,38 +1,58 @@
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 /**
  * DataEngine - Main Class for the application, Use this to generate our objects in the global scope
- * @author Sean Domingo, Michael Frederick, Megan Molumby, Mai Huong Nguyen, Richard Pratt
+ *
+ * @author Sean Domingo, Michael Frederick, Megan Molumby, Mai Huong Nguyen,
+ * Richard Pratt
  */
-import java.util.ArrayList;
 
 public class DataEngine {
+
     private SQLParser sqlParser;
     private FileReader fileReader;
     private UserInterface userInterface;
     private XMLToSQLGenerator xmlToSqlGenerator;
     private XMLToSQLParser xmlToSqlParser;
-    
+
     public DataEngine() {
         this.sqlParser = new SQLParser();
         this.fileReader = new FileReader();
         this.userInterface = new UserInterface();
         this.xmlToSqlGenerator = new XMLToSQLGenerator();
         this.xmlToSqlParser = new XMLToSQLParser();
+
+        checkFilesystem();
     }
-    
+
     /**
      * Actual entry point
      */
     public void runEngine() {
-        System.out.println("Running");
+        //System.out.println("Running");
         //calls XmlParser and parses given textfile
-        xmlToSQLParser();
+        //xmlToSQLParser();
 
     }
-    public void xmlToSQLParser(){
-        fileReader = new FileReader();
-        fileReader.setInputFile();
-        xmlToSqlParser = new XMLToSQLParser();
-        xmlToSqlParser.parse(fileReader.getInputFile());
 
+    public void xmlToSQLParser() {
+        this.fileReader.setInputFile();
+        this.xmlToSqlParser.parse(this.fileReader.getInputFile());
+    }
+
+    private void checkFilesystem() {
+        Path path = Paths.get("tables\\");
+        if (Files.notExists(path)) {
+            // Initialize Tables directory
+        }
+        
+        path = Paths.get("databases\\");
+        if (Files.notExists(path)) {
+            // Initialize database directory
+        }
     }
 }
